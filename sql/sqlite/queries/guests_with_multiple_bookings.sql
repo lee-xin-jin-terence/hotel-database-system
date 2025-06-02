2602a10b0c0856bd03b38d8670105ba640a69cd1
terence-lee-xin-jin <89919135+lee-xin-jin-terence@users.noreply.github.com>
2025-06-02 14:37:55 +0800

    Create guests_with_multiple_bookings.sql

---
+++ sql/sqlite/queries/guests_with_multiple_bookings.sql
@@ -0,0 +1,13 @@
+-- Query: Find guests with multiple bookings
+-- Description: Retrieves guests who have made more than one booking by counting the number of bookings associated with each guest.
+-- Use Case: Useful for identifying frequent customers, enabling targeted marketing, loyalty programs, or personalized services.
+
+SELECT
+    g.guestId,
+    g.firstName,
+    g.lastName,
+    COUNT(bg.bookingId) AS bookingCount
+FROM Guest g
+JOIN BookingGuest bg ON g.guestId = bg.guestId
+GROUP BY g.guestId, g.firstName, g.lastName
+HAVING COUNT(bg.bookingId) > 1;
